f74ddf7432cc29c29b9956be28b65dab73a012a0
shahin2525 <shahin451999@gmail.com>
2025-02-24 14:25:50 -0800

    add generate static

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,4 +1,13 @@
 import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
+import { Blog } from "@/types";
+
+export const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+  return blogs?.slice(0, 3).map((blog: Blog) => ({
+    blogId: blog.id,
+  }));
+};
 
 const page = async ({ params }: { params: Promise<{ blogId: string }> }) => {
   const { blogId } = await params;
@@ -9,8 +18,7 @@ const page = async ({ params }: { params: Promise<{ blogId: string }> }) => {
 
   return (
     <div>
-      {" "}
-      <BlogDetailsCard blog={blog} />{" "}
+      <BlogDetailsCard blog={blog} />
     </div>
   );
 };
